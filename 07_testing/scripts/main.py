5c248d2a7c1acd93b564dacf95419c67e736af5d
sinan <sinan@test.tmp>
2013-03-17 21:28:07 +0100

    created 07 project

---
+++ 07_testing/scripts/main.py
@@ -0,0 +1,9 @@
+import engine_scripts.createobjects as create
+
+def init(Engine,EngineModule,objects):
+	pass
+	#print("create spacecage")
+	#create.createSpaceCage(Engine,EngineModule,
+	#	EngineModule.Vec3(100,200,200))
+
+
---
+++ 07_testing/scripts/temp.py
@@ -0,0 +1,31 @@
+"""temp:
+	,:
+	.:
+	/:
+"""
+import engine_scripts.createobjects as create
+import engine_scripts.helpers as helpers
+import engine_scripts.saveload as saveload
+
+import random
+import datetime
+
+import pyscript_00.ragdoll as ragdoll
+
+def keyPressed(Engine,EngineModule,key,selection,objects):
+	if key == EngineModule.Keys.K_COMMA:
+		pass
+		print("save to xml")
+		saveload.save(Engine,EngineModule,"../xml/test.xml",objects)
+		print("done")
+
+	if key == EngineModule.Keys.K_PERIOD:
+		pass
+		print("load from xml")
+		saveload.load(Engine,EngineModule,"../xml/test.xml",objects)
+		print("done")
+
+	if key == EngineModule.Keys.K_SLASH:
+		pass
+
+		
